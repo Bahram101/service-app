7c7dc139573fdc63dad3c858dbe5f9d5e7b99f2f
Bahrom <bahram8787@gmail.com>
2025-07-23 18:51:51 +0500

    react react-dom same varsion

--- App.tsx
+++ App.tsx
@@ -1,15 +1,20 @@
-import { Text, View } from 'react-native'
+import { View } from 'react-native'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
+import Home from '@/components/screens/home/Home'
 import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider'
 
 import './global.css'
+import { StatusBar } from 'expo-status-bar'
+import Navigation from '@/navigation/Navigation'
 
 export default function App() {
   return (
     <GluestackUIProvider mode='light'>
-      <View className='flex flex-col items-center justify-center bg-red-100 h-full'>
-        <Text className='bg-blue-100'>Open app.tsx!</Text>
-      </View>
+      <SafeAreaProvider>
+        <Navigation />
+        <StatusBar style='auto' />
+      </SafeAreaProvider>
     </GluestackUIProvider>
   )
 }
---
+++ app/navigation/Navigation.tsx
@@ -0,0 +1,22 @@
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import React from 'react'
+
+import { TypeRootStackParamList } from './navigation.types'
+import { routes } from './routes'
+
+const Stack = createNativeStackNavigator<TypeRootStackParamList>()
+
+const Navigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {routes.map(route => (
+          <Stack.Screen key={route.name} {...route} />
+        ))}
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default Navigation
