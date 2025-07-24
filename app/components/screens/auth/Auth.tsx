4081c9111d4a7f5ca6facc5c3efc0253f1de4814
Bahrom <bahram8787@gmail.com>
2025-07-24 16:25:56 +0500

    add navigation type

--- app/components/screens/auth/Auth.tsx
+++ app/components/screens/auth/Auth.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { use } from 'react'
 import { Text, View } from 'react-native'
 
+import { Button } from '@/components/ui/button'
+import { useTypedNavigation } from '@/hooks/useTypedNavigation'
+
 const Auth = () => {
+  const { navigate } = useTypedNavigation()
+
   return (
     <View>
       <Text>Auth</Text>
+      <Button onPress={() => navigate('Home')}>
+        <Text className='text-white'>Home</Text>
+      </Button>
     </View>
   )
 }
