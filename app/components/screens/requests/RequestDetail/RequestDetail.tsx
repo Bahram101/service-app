bf97cebb3ef3b55f2c60b4704d0ff404e45b9187
Bahrom <bahram8787@gmail.com>
2025-09-06 12:06:35 +0500

    continue baseAccordion

--- app/components/screens/requests/RequestDetail/RequestDetail.tsx
+++ app/components/screens/requests/RequestDetail/RequestDetail.tsx
@@ -1,29 +1,12 @@
 import { Feather } from '@expo/vector-icons'
 import cn from 'clsx'
-import React, { useRef } from 'react'
-import { Alert, Animated, Pressable, Text, View } from 'react-native'
+import React from 'react'
+import { Pressable, ScrollView, Text, View } from 'react-native'
 
 import Layout from '@/components/layout/Layout'
 import Heading from '@/components/ui/Heading'
-import {
-  Accordion,
-  AccordionContent,
-  AccordionContentText,
-  AccordionHeader,
-  AccordionIcon,
-  AccordionItem,
-  AccordionTitleText,
-  AccordionTrigger
-} from '@/components/ui/accordion'
 import BaseAccordion from '@/components/ui/accordion/BaseAccardion'
 import AnimatedPressable from '@/components/ui/button/AnimatedPressable'
-import { Divider } from '@/components/ui/divider'
-import {
-  ChevronDownIcon,
-  ChevronUpIcon,
-  Icon,
-  TrashIcon
-} from '@/components/ui/icon'
 
 import { useTypedNavigation } from '@/hooks/useTypedNavigation'
 
@@ -31,9 +14,7 @@ import { open2GIS } from '@/services/maps/open2gis'
 
 import { COLORS } from '@/constants/colors'
 
-type Props = {}
-
-const RequestDetail = (props: Props) => {
+const RequestDetail = () => {
   const { goBack, navigate } = useTypedNavigation()
 
   const request = {
@@ -68,125 +49,76 @@ const RequestDetail = (props: Props) => {
   return (
     <Layout>
       <Heading backIcon={true}>Заявка №1564654</Heading>
+      <ScrollView 
+      className='min-h-full'
+        showsVerticalScrollIndicator={true}
+        contentContainerStyle={{ paddingBottom: 70, flexGrow: 1 }}
+      >
+        <View className='px-4 h-full pt-3 gap-3'>
+          <BaseAccordion title='Данные клиента' icon='user'>
+            <Text>{request.client.name}</Text>
+            <Text>{request.client.address}</Text>
+            <Text>{request.client.problem}</Text>
+          </BaseAccordion>
 
-      <View className='px-4 h-full pt-3 gap-3'>
-        <BaseAccordion title='Данные клиента'>
-          <Text>{request.client.name}</Text>
-          <Text>{request.client.address}</Text>
-          <Text>{request.client.problem}</Text>
-        </BaseAccordion>
+          <BaseAccordion title='Вид сервиса' icon='settings'>
+            <Text>{request.service[0]}</Text>
+            <Text>{request.service[1]}</Text>
+            <Text>{request.service[2]}</Text>
+          </BaseAccordion>
 
-        <View className='bg-white p-4 rounded-2xl'>
-          <View className='flex-row items-center border-b pb-4 border-grayLight'>
-            <Feather name='user' size={22} style={{ color: '#15803d' }} />
-            <Text className='font-bold ml-2 uppercase text-primary '>
-              КЛИЕНТ
-            </Text>
-          </View>
-          <Text className='text-lg mt-3 pl-3 border-l-4 border-primary ml-2'>
-            Асрор Умаров
-          </Text>
-        </View>
+          <BaseAccordion title='Данные аппарата' icon='alert-circle'>
+            <Text>{request.service[0]}</Text>
+            <Text>{request.service[1]}</Text>
+            <Text>{request.service[2]}</Text>
+          </BaseAccordion>
 
-        <View className='bg-white p-4 rounded-2xl'>
-          <View className='flex-row items-center border-b pb-4 border-grayLight'>
-            <Feather name='map-pin' size={22} style={{ color: '#15803d' }} />
-            <Text className='font-bold ml-2 uppercase text-primary'>АДРЕС</Text>
-          </View>
-          <View className='flex-row items-center justify-between pt-4'>
-            <Text className='text-lg border-l-4 border-primary pl-3 ml-2'>
-              Мкр. Аксай-4, дом 96, кв 10 Мкр.
-            </Text>
-            <Pressable onPressIn={() => open2GIS()}>
-              {({ pressed }) => (
-                <Feather
-                  name='external-link'
-                  size={22}
-                  style={[
-                    { color: pressed ? COLORS.grayLight : '#007AFF' },
-                    pressed &&
-                      {
-                        // backgroundColor: COLORS.grayDark,
-                        // padding: 3,
-                        // borderRadius: '7px'
-                      }
-                  ]}
-                />
-              )}
-            </Pressable>
-          </View>
-        </View>
-
-        <View className='bg-white p-4 rounded-2xl'>
-          <View className='flex-row items-center border-b pb-4 border-grayLight'>
-            <Feather name='clock' size={22} style={{ color: '#15803d' }} />
-            <Text className='font-bold ml-2 uppercase text-primary'>
-              ИСТОРИЯ ОБСЛУЖИВАНИЕ
-            </Text>
-          </View>
-          {request.history.map((history, index) => (
-            <View
-              key={history.id}
-              className={cn(
-                'flex-row items-center justify-between pt-4 border-grayLight',
-                request.length - 1 !== index && 'border-b pb-3'
-              )}
-            >
-              <Text className='border-l-4 border-primary pl-3 ml-2'>
-                {history.date}
-              </Text>
-              <Pressable onPress={() => navigate('Requests')}>
-                <Feather name='chevron-right' size={22} />
-              </Pressable>
+          <View className='gap-3'>
+            <View className='flex-row gap-3'>
+              <AnimatedPressable
+                bg={COLORS.blue}
+                bgPressed={COLORS.blueDark}
+                className='flex-1'
+              >
+                <View className='flex-row gap-3 items-center'>
+                  <Feather
+                    name='message-circle'
+                    size={30}
+                    style={{ color: 'white' }}
+                  />
+                  <View className='flex-col items-center'>
+                    <Text className='font-bold text-white'>Чат с</Text>
+                    <Text className='font-bold text-white'>клиентом</Text>
+                  </View>
+                </View>
+              </AnimatedPressable>
+              <AnimatedPressable
+                bg={COLORS.primary}
+                bgPressed={COLORS.primaryDark}
+                className='flex-1'
+              >
+                <View className='flex-row gap-3 items-center'>
+                  <Feather
+                    name='phone-call'
+                    size={30}
+                    style={{ color: 'white' }}
+                  />
+                  <View className='flex-col items-center'>
+                    <Text className='font-bold text-white'>Позвонить</Text>
+                    <Text className='font-bold text-white'>клиенту</Text>
+                  </View>
+                </View>
+              </AnimatedPressable>
             </View>
-          ))}
-        </View>
-
-        <View className='gap-3'>
-          <View className='flex-row gap-3'>
-            <AnimatedPressable
-              bg={COLORS.blue}
-              bgPressed={COLORS.blueDark}
-              className='flex-1'
-            >
+            <AnimatedPressable bg={COLORS.yellow} bgPressed={COLORS.yellowDark}>
               <View className='flex-row gap-3 items-center'>
-                <Feather
-                  name='message-circle'
-                  size={30}
-                  style={{ color: 'white' }}
-                />
-                <View className='flex-col items-center'>
-                  <Text className='font-bold text-white'>Чат с</Text>
-                  <Text className='font-bold text-white'>клиентом</Text>
-                </View>
-              </View>
-            </AnimatedPressable>
-            <AnimatedPressable
-              bg={COLORS.primary}
-              bgPressed={COLORS.primaryDark}
-              className='flex-1'
-            >
-              <View className='flex-row gap-3 items-center'>
-                <Feather
-                  name='phone-call'
-                  size={30}
-                  style={{ color: 'white' }}
-                />
-                <View className='flex-col items-center'>
-                  <Text className='font-bold text-white'>Позвонить</Text>
-                  <Text className='font-bold text-white'>клиенту</Text>
-                </View>
+                <Feather name='map' size={24} color='black' />
+                <Text className='text-black font-bold'>Принять</Text>
               </View>
             </AnimatedPressable>
           </View>
-          <AnimatedPressable bg={COLORS.yellow} bgPressed={COLORS.yellowDark}>
-            <View className='flex-row gap-3 items-center'>
-              <Feather name='map' size={24} color='black' />
-              <Text className='text-black font-bold'>Принять</Text>
-            </View>
-          </AnimatedPressable>
         </View>
-      </View>
+      </ScrollView>
     </Layout>
   )
 }
--- app/components/ui/accordion/BaseAccardion.tsx
+++ app/components/ui/accordion/BaseAccardion.tsx
@@ -1,4 +1,7 @@
-import React, { FC, ReactNode } from 'react'
+import { Feather } from '@expo/vector-icons'
+import cn from 'clsx'
+import React, { FC, ReactNode, useState } from 'react'
+import { Text, View } from 'react-native'
 
 import {
   Accordion,
@@ -11,14 +14,15 @@ import {
   AccordionTrigger
 } from '@/components/ui/accordion'
 import { ChevronDownIcon, ChevronUpIcon } from '@/components/ui/icon'
-import { View } from 'react-native'
+import { TypeFeatherIconNames } from '@/types/icon.interface'
 
 type BaseAccordionProps = {
   title: string
   children: ReactNode
+  icon: TypeFeatherIconNames
 }
 
-const BaseAccordion: FC<BaseAccordionProps> = ({ title, children }) => {
+const BaseAccordion: FC<BaseAccordionProps> = ({ title, children, icon }) => {
   return (
     <Accordion
       size='md'
@@ -26,28 +30,42 @@ const BaseAccordion: FC<BaseAccordionProps> = ({ title, children }) => {
       type='single'
       isCollapsible={true}
       isDisabled={false}
-      className='rounded-2xl'
+      className='rounded-2xl px-4'
     >
-      <AccordionItem value='a' className='rounded-2xl'>
-        <AccordionHeader>
-          <AccordionTrigger>
-            {({ isExpanded }: { isExpanded: boolean }) => {
-              return (
-                <>
-                  <AccordionTitleText>{title}</AccordionTitleText>
-                  {isExpanded ? (
-                    <AccordionIcon as={ChevronUpIcon} className='ml-3' />
-                  ) : (
-                    <AccordionIcon as={ChevronDownIcon} className='ml-3' />
-                  )}
-                </>
-              )
-            }}
-          </AccordionTrigger>
-        </AccordionHeader>
-        <AccordionContent>
-          <AccordionContentText className=''>
-            <View className='gap-3'>{children}</View>
+      <AccordionItem value='a' className='rounded-2xl '>
+        <AccordionTrigger className='pb-3 p-0'>
+          {({ isExpanded }: { isExpanded: any }) => {
+            return (
+              <AccordionHeader
+                className={cn(
+                  'w-full flex-row justify-between py-3 border-grayLight ',
+                  isExpanded && 'border-b'
+                )}
+              >
+                <View className='flex-row gap-2 items-center  '>
+                  <Feather name={icon} size={22} style={{ color: '#15803d' }} />
+                  <AccordionTitleText className='text-primary uppercase '>
+                    {title}
+                  </AccordionTitleText>
+                </View>
+                {isExpanded ? (
+                  <AccordionIcon
+                    as={ChevronUpIcon}
+                    className='mr-2 right-4 top-1'
+                  />
+                ) : (
+                  <AccordionIcon
+                    as={ChevronDownIcon}
+                    className='mr-2 right-4 top-1'
+                  />
+                )}
+              </AccordionHeader>
+            )
+          }}
+        </AccordionTrigger>
+        <AccordionContent className='p-0 py-3'>
+          <AccordionContentText className='pl-[9px]'>
+            <View className='gap-3 border-l-4 border-primary pl-3'>{children}</View>
           </AccordionContentText>
         </AccordionContent>
       </AccordionItem>
